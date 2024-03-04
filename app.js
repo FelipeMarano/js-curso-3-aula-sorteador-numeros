f1c025cf57c1d501844a628eaab79b1a68928bc1
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-04 16:01:57 -0300

    add ids no js e alerts

---
+++ app.js
@@ -0,0 +1,8 @@
+function sortear () { // puxando os Ids para dentro do javascript
+    let quantidade = document.getElementById ("quantidade").value; // para pegar o valor que vai ser digitado
+    let de = document.getElementById ("de").value;
+    let ate = document.getElementById ("ate").value;
+    alert (`Quantidade ${quantidade}`); // alert apenas para testar se esta funcionando
+    alert (`de ${de}`);
+    alert (`ate ${ate}`);
+}
