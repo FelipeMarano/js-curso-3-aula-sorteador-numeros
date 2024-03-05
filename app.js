f19c865eab6ad82a4cf89b7df7339c38e849c6cb
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-05 13:25:30 -0300

    função para sortear

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 function sortear () { // puxando os Ids para dentro do javascript
-    let quantidade = document.getElementById ("quantidade").value; // para pegar o valor que vai ser digitado
-    let de = document.getElementById ("de").value;
-    let ate = document.getElementById ("ate").value;
-    alert (`Quantidade ${quantidade}`); // alert apenas para testar se esta funcionando
-    alert (`de ${de}`);
-    alert (`ate ${ate}`);
+    let quantidade = parseInt (document.getElementById ("quantidade").value); // para pegar o valor que vai ser digitado
+    let de = parseInt(document.getElementById ("de").value); // parseInt para vir somente numero
+    let ate = parseInt (document.getElementById ("ate").value);
+    
+    let sorteados = []; 
+    let numero; 
+    // fazer um "loop for"
+    for (let i = 0; i < quantidade; i++) { // começa com 0; enquanto ela for menor que a "quantidade", continua e ; vai aumentando
+        numero = obterNumeroAleatorio (de,ate);
+        sorteados.push(numero);
+    }
+
+    alert (sorteados)
+    
+}
+
+function obterNumeroAleatorio (min,max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min; // Math.floor arredonda os numeros
 }
