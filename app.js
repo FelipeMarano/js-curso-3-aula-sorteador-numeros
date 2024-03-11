f065d5b892a7548ebc98c02d4548b2940ff8e541
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-11 13:29:20 -0300

    status botao e funcao reiniciar

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-function sortear () { // puxando os Ids para dentro do javascript
-    let quantidade = parseInt (document.getElementById ("quantidade").value); // para pegar o valor que vai ser digitado
-    let de = parseInt(document.getElementById ("de").value); // parseInt para vir somente numero
+function sortear () { 
+    let quantidade = parseInt (document.getElementById ("quantidade").value); 
+    let de = parseInt(document.getElementById ("de").value); 
     let ate = parseInt (document.getElementById ("ate").value);
     
-    let sorteados = []; // array
+    let sorteados = []; 
     let numero; 
-    // fazer um "loop for"
-    for (let i = 0; i < quantidade; i++) { // começa com 0; enquanto ela for menor que a "quantidade", continua e ; vai aumentando
+    
+    for (let i = 0; i < quantidade; i++) { 
         numero = obterNumeroAleatorio (de,ate);
-        while (sorteados.includes(numero)); // repetir o codigo se a condição for verdadeira; includes: devolve true or false
-            numero = obterNumeroAleatorio (de,ate);
-        sorteados.push(numero); // para guardar dentro do array
+        while (sorteados.includes(numero));
+        numero = obterNumeroAleatorio (de,ate);
+        sorteados.push(numero); 
     }
 
-    let resultado = document.getElementById ("resultado"); // para pegar o "resultado" do index
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`; // para colocar no HTML e concatenar
-    
+    let resultado = document.getElementById ("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`; 
+    alterarStatusBotao ();
 }
 
 function obterNumeroAleatorio (min,max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min; // Math.floor arredonda os numeros
+   return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
+function alterarStatusBotao () {
+    let botao =  document.getElementById ("btn-reiniciar"); 
+    if (botao.classList.contains("container__botao-desabilitado")) {; 
+        botao.classList.remove ("container__botao-desabilitado");  
+        botao.classList.add ("container__botao");
+    } else {
+        botao.classList.remove ("container__botao");
+        botao.classList.add ("container__botao-desabilitado");
+    }
+}
+
+function reiniciar () {  
+    document.getElementById ("quantidade").value = " "; 
+    document.getElementById ("de").value = " "; 
+    document.getElementById ("ate").value = " ";
+    document.getElementById ("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao (); 
+}
